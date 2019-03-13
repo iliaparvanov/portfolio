c833e89f6ccd8a35ab41c6cc67aee79262848479
iliapa1 <ilia.nikolaev.parvanov@gmail.com>
2019-03-13 13:01:34 +0200

    Added basic animations and sticky navbar

--- js/script.js
+++ js/script.js
@@ -1,3 +1,34 @@
+//Init SmoothScroll
 const scroll = new SmoothScroll('.hero a[href*="#"]', {
 	speed: 800
 });
+
+var controller = new ScrollMagic.Controller();
+
+if(window.innerWidth > 1024) {
+	var pinNav = new ScrollMagic.Scene({
+		triggerHook: 'onLeave',
+		triggerElement: '#nav'
+	})
+	.setPin('#nav', {pushFollowers: false})
+	.addTo(controller);
+
+	var pinContactMe = new ScrollMagic.Scene({
+		triggerHook: 'onLeave',
+		triggerElement: '#contactme'
+	})
+	.setPin('#contactme', {pushFollowers: false})
+	.addTo(controller);
+}
+
+var allSections = document.getElementsByClassName("section");
+for (let section of allSections) {
+	let scene = new ScrollMagic.Scene({
+		triggerElement: section,
+		reverse: false
+	})
+	.setClassToggle(section, 'fade-in')
+	.addIndicators()
+	.addTo(controller);
+}
+
