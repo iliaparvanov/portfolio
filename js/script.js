67f20a45e0c4576d5a185bb9b3ec5d50b7d1ae72
iliapa1 <ilia.nikolaev.parvanov@gmail.com>
2019-03-13 14:47:22 +0200

    Fixed navbar which is now sticky and beauuuutiful:

--- js/script.js
+++ js/script.js
@@ -5,12 +5,20 @@ const scroll = new SmoothScroll('.hero a[href*="#"]', {
 
 var controller = new ScrollMagic.Controller();
 
-if(window.innerWidth > 1024) {
+/*if(window.innerWidth > 1024) {
 	var pinNav = new ScrollMagic.Scene({
 		triggerHook: 'onLeave',
 		triggerElement: '#nav'
 	})
 	.setPin('#nav', {pushFollowers: false})
+	.setClassToggle('#nav', 'has-background-white')
+	.addTo(controller);
+
+	var changeTextColor = new ScrollMagic.Scene({
+		triggerHook: 'onLeave',
+		triggerElement: '#nav'
+	})
+	.setClassToggle('#nav', 'has-text-black')
 	.addTo(controller);
 
 	var pinContactMe = new ScrollMagic.Scene({
@@ -19,7 +27,15 @@ if(window.innerWidth > 1024) {
 	})
 	.setPin('#contactme', {pushFollowers: false})
 	.addTo(controller);
-}
+}*/
+
+var blackNavbar = new ScrollMagic.Scene({
+	triggerHook: 'onLeave',
+	triggerElement: '#imilia'
+})
+.setClassToggle('.navbar', 'has-background-dark')
+.addTo(controller);
+
 
 var allSections = document.getElementsByClassName("section");
 for (let section of allSections) {
