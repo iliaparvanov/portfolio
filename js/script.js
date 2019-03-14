8552f455f836c8221398673d360b16fb5c1f8883
iliapa1 <ilia.nikolaev.parvanov@gmail.com>
2019-03-14 23:48:54 +0200

    Nvm, had to fix some javascript

--- js/script.js
+++ js/script.js
@@ -5,30 +5,6 @@ const scroll = new SmoothScroll('.hero a[href*="#"]', {
 
 var controller = new ScrollMagic.Controller();
 
-/*if(window.innerWidth > 1024) {
-	var pinNav = new ScrollMagic.Scene({
-		triggerHook: 'onLeave',
-		triggerElement: '#nav'
-	})
-	.setPin('#nav', {pushFollowers: false})
-	.setClassToggle('#nav', 'has-background-white')
-	.addTo(controller);
-
-	var changeTextColor = new ScrollMagic.Scene({
-		triggerHook: 'onLeave',
-		triggerElement: '#nav'
-	})
-	.setClassToggle('#nav', 'has-text-black')
-	.addTo(controller);
-
-	var pinContactMe = new ScrollMagic.Scene({
-		triggerHook: 'onLeave',
-		triggerElement: '#contactme'
-	})
-	.setPin('#contactme', {pushFollowers: false})
-	.addTo(controller);
-}*/
-
 var blackNavbar = new ScrollMagic.Scene({
 	triggerHook: 'onLeave',
 	triggerElement: '#imilia'
@@ -44,7 +20,6 @@ for (let section of allSections) {
 		reverse: false
 	})
 	.setClassToggle(section, 'fade-in')
-	.addIndicators()
 	.addTo(controller);
 }
 
